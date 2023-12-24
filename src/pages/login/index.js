3227896ba6f8460ddac898ba075f7c2f63442623
Anderson Inacio <anderson.mouratoo@gmail.com>
2023-12-24 15:59:28 -0300

    save

---
+++ src/pages/login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Title = styled.div`
+    background-color: red;
+    padding:20px;
+`
+
+
+const Login = ()=>{
+    return(
+        <Title>
+            <h1> Olá de Volta! </h1>    
+        </Title>
+        )
+    }
+export default Login;
